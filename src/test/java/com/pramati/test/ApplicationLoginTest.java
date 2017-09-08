b9a9c6c896e7b2f76d6ab8a15e0139465cd56b2c
Maheswarareddy Gade <maheswara.gade@imaginea.com>
2017-09-08 18:20:22 +0530

    te test commit: :wq

--- src/test/java/com/pramati/test/ApplicationLoginTest.java
+++ src/test/java/com/pramati/test/ApplicationLoginTest.java
@@ -50,44 +50,7 @@ public class ApplicationLoginTest {
 		
 		return false;
 
-		/*if (objGenericLib.isElementVisible(ObjloginPage.getNextButton())) {
-			Log.Info("Application invoke successful.");
-		} else {
-			Log.Fail("Application invoke failed.");
-			return false;
-		}
-		try {
-			objGenericLib.clearAndEnterText(ObjloginPage.getUserName(), sUserName);
-			objGenericLib.clickAndWait(ObjloginPage.getNextButton(), ThreadTimeOut.ONE_SECOND);
-			objGenericLib.threadSleep(ThreadTimeOut.ONE_SECOND);
-			if (!objGenericLib.isElementVisible(ObjloginPage.getPassword())) {
-				if(objGenericLib.isElementPresentByXpath("//*[text()='Please enter a valid email address. ']")){
-					Log.Fail("Application login failed. Because of Invalid User Id.");
-					return false;
-				}
-			}			
-
-			objGenericLib.clearAndEnterText(ObjloginPage.getPassword(), sPassword);
-
-			objGenericLib.clickAndWait(ObjloginPage.getSignInButton(), ThreadTimeOut.ONE_SECOND);
-			objGenericLib.threadSleep(ThreadTimeOut.ONE_SECOND);
-
-			ApplicationLogin.sHomeWindowId = objGenericLib.getWindow();
-
-			if(objGenericLib.isElementPresentByXpath("//span[contains(text(),\"The email and password you entered don't match\")]")){
-				Log.Fail("Application login failed. The email and password you entered don't match.");
-				return false;
-			}else if(objGenericLib.isElementPresentByXpath("//*[@class='error-msg']")){
-				if(objGenericLib.getWebElement("//*[@class='error-msg']").isDisplayed()){
-					Log.Fail("Application login failed. " + objGenericLib.getWebElementText("//*[@class='error-msg']"));
-					return false;
-				}
-			}
-			return true;
-		} catch (Exception e) {
-			Log.Fail("Unable to enter login details, Please refer screenshot");
-			return false;
-		}*/
+		
 	}
 
 	/**
